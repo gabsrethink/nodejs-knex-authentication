c1e1a8af6b9d24fa68dcad9776263ee32c27ac55
gabsrethink <gabriel.melo@rethink.dev>
2022-10-28 11:05:32 -0300

    teste de função

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
 import { app } from '../index';
 import UserRepository from '../repositories/UserRepository';
+import { login } from '../services/user.service';
 
 describe('User routes', () => {
   test('/signup', async () => {
-    jest.spyOn(UserRepository, 'register').mockResolvedValue([14]);
+    jest.spyOn(UserRepository, 'register').mockResolvedValue([24]);
     const res = await request(app).post('/signup').send({
       name: 'Gabriel',
       email: 'gabriel@rethink.com',
@@ -13,7 +14,7 @@ describe('User routes', () => {
     });
     expect(res.status).toEqual(200);
   });
-  test('/signup', async () => {
+  test('/signup same e-mail error', async () => {
     jest
       .spyOn(UserRepository, 'register')
       .mockRejectedValue({ code: 'ER_DUP_ENTRY' });
@@ -24,5 +25,22 @@ describe('User routes', () => {
       confirmPassword: 'Gabriel1991!',
     });
     expect(res.status).toEqual(409);
+  });
+});
+
+describe('Service functions', () => {
+  test('200 - successful login ', async () => {
+    jest.spyOn(UserRepository, 'getUser').mockResolvedValue({
+      id: 1,
+      name: 'Gabriel Melo',
+      email: 'gabriel.melo@rethink.dev',
+      password: '$2b$08$3jEG0ZUfd0j/E3K/vkHy5u/mVMWPnTV98zt7fMVy.XOjiiGSHw6Qa',
+    });
+    const user = {
+      email: 'gabriel.melo@rethink.dev',
+      password: 'batata',
+    };
+    const res = await login(user.email, user.password);
+    expect(res).toContain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
   });
 });
